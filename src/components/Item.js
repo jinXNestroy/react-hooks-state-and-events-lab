f58540a342b1c3d008c9a77221bf7a2e15c1d031
DanielJoel <danpersie53@gmail.com>
2024-05-23 22:19:59 +0300

    state lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,32 @@
 import React from "react";
+import { useState } from "react";
+
 
 function Item({ name, category }) {
+  const [cart, setCart] = useState("")
+  const [buttonDisplay, setButtonDisplay] = useState("Add to Cart")
+
+  function handleClick() {
+
+
+
+    setCart((prevcart) => {
+      if (prevcart === "") {
+        setButtonDisplay("Remove From Cart")
+        return "in-cart"
+      }
+      else {
+        setButtonDisplay("Add to Cart")
+        return ""
+      }
+    })
+  }
+
   return (
-    <li className="">
+    <li className={cart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>{buttonDisplay}</button>
     </li>
   );
 }
